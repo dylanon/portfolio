c923e8c43d766fe20875a9f94ff7fca6f5e26c3f
Dylan On <dylanon@users.noreply.github.com>
2017-12-06 17:47:56 -0500

    Move index.html into the dev folder, pipe and watch for changes with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,33 @@ const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
 
+gulp.task('markup', () => {
+    return gulp.src('dev/*.html')
+        .pipe(gulp.dest('public'));
+});
+
 gulp.task('assets', () => {
-	return gulp.src('./dev/assets/*')
-	.pipe(gulp.dest('./public/assets'))
+	return gulp.src('dev/assets/*')
+	.pipe(gulp.dest('public/assets'))
 });
 
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/styles'))
+		.pipe(gulp.dest('public/styles'))
 });
 
 gulp.task('bs', () => {
 	return browserSync.init({
 		server: {
-			baseDir: './'
+			baseDir: 'public'
 		}
 	});
 });
 
-gulp.task('default', ['styles','assets','bs'], () => {
-	gulp.watch('dev/**/*.scss',['styles']);
-	gulp.watch('./public/styles/style.css',reload);
+gulp.task('default', ['markup','styles','assets','bs'], () => {
+    gulp.watch('dev/**/*.scss',['styles']);
+    gulp.watch('dev/*.html', ['markup', reload]);
+	gulp.watch('public/styles/style.css', reload);
 });
