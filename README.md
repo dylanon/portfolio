2f67bcac04cb34ba44b7de06afbaaaee659ea96e
Dylan On <dylanon@users.noreply.github.com>
2017-12-06 15:17:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+Portfolio website for [Dylan On, web developer](https://dylanon.com).
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
+
+gulp.task('assets', () => {
+	return gulp.src('./dev/assets/*')
+	.pipe(gulp.dest('./public/assets'))
+});
+
+gulp.task('styles', () => {
+	return gulp.src('./dev/styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/styles'))
+});
+
+gulp.task('bs', () => {
+	return browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+gulp.task('default', ['styles','assets','bs'], () => {
+	gulp.watch('dev/**/*.scss',['styles']);
+	gulp.watch('./public/styles/style.css',reload);
+});
