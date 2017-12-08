02e1b2041796cafc33e2ba1a0370af8600ef6849
Dylan On <dylanon@users.noreply.github.com>
2017-12-07 22:10:06 -0500

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const reload = browserSync.reload;
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('markup', () => {
     return gulp.src('dev/*.html')
@@ -19,6 +20,7 @@ gulp.task('assets', () => {
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('public/styles'))
 });
