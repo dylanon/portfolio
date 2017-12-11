a8624e872d42609146993cc24bbb8e7d0637431e
Dylan On <dylanon@users.noreply.github.com>
2017-12-11 14:02:34 -0500

    Add smoothScroll

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,11 +1,22 @@
 import $ from 'jquery';
-import AOS from 'aos'
+import smoothScroll from 'jquery-smooth-scroll';
+import AOS from 'aos';
+
+function closeNav() {
+    $('.main-nav-links').removeClass('nav-active');
+    $('.logo').removeClass('nav-active');
+}
 
 $(function() {
     // Toggle class for CSS transition when mobile nav toggle is pressed
     $('.nav-toggle').on('click', function() {
         $('.main-nav-links').toggleClass('nav-active');
         $('.logo').toggleClass('nav-active');
+    });
+
+    $('.main-nav-links a').smoothScroll({
+        beforeScroll: closeNav,
+        speed: 600
     });
 
     // Initialize Animate On Scroll
