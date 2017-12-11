2d084b9db3e6a94741962a2d7956cd1723ff5473
Dylan On <dylanon@users.noreply.github.com>
2017-12-11 12:14:27 -0500

    Formatting

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,13 +1,14 @@
 import $ from 'jquery';
 import AOS from 'aos'
 
-// Toggle class for CSS transition when mobile nav toggle is pressed
 $(function() {
+    // Toggle class for CSS transition when mobile nav toggle is pressed
     $('.nav-toggle').on('click', function() {
         $('.main-nav-links').toggleClass('nav-active');
         $('.logo').toggleClass('nav-active');
     });
 
+    // Initialize Animate On Scroll
     AOS.init({
         duration: 700
     });
