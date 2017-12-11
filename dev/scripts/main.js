a0b8b63eb243b7e56c9dabfc540b748284a89662
Dylan On <dylanon@users.noreply.github.com>
2017-12-11 12:13:58 -0500

    Animate portfolio items, add AOS to project

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,9 +1,14 @@
 import $ from 'jquery';
+import AOS from 'aos'
 
 // Toggle class for CSS transition when mobile nav toggle is pressed
 $(function() {
     $('.nav-toggle').on('click', function() {
         $('.main-nav-links').toggleClass('nav-active');
         $('.logo').toggleClass('nav-active');
+    });
+
+    AOS.init({
+        duration: 700
     });
 });
