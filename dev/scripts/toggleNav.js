1f161c891bd070be37392135903f4c65f3861049
Dylan On <dylanon@users.noreply.github.com>
2017-12-11 23:20:12 -0500

    Make mobile nav toggle accessible

--- dev/scripts/toggleNav.js
+++ dev/scripts/toggleNav.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 
 export default function toggleNav() {
     // Toggle class for CSS transition when mobile nav toggle is pressed
-    $('.nav-toggle').on('click', function() {
+    $('.nav-toggle a').on('click', function(e) {
+        e.preventDefault();
         $('.main-nav-links').toggleClass('nav-active');
         $('.logo').toggleClass('nav-active');
     });
