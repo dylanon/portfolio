b851c8791f9c342f8e6738249e43512e3064131c
Dylan On <dylanon@users.noreply.github.com>
2017-12-12 15:32:42 -0500

    Fix bug where nav toggle was reversing its state

--- dev/scripts/closeNav.js
+++ dev/scripts/closeNav.js
@@ -3,4 +3,5 @@ import $ from 'jquery';
 export default function closeNav() {
     $('.main-nav-links').removeClass('nav-active');
     $('.logo').removeClass('nav-active');
+    $('.toggle-bar').removeClass('nav-active');
 }
