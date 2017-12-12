15b1d98af6fabc60dc7abc251ff0c9d980480c27
Dylan On <dylanon@users.noreply.github.com>
2017-12-11 23:48:55 -0500

    Copy improvements, add smooth scrolling to bio contact link

--- dev/scripts/smoothScrollLinks.js
+++ dev/scripts/smoothScrollLinks.js
@@ -3,8 +3,14 @@ import smoothScroll from 'jquery-smooth-scroll';
 import closeNav from './closeNav';
 
 export default function smoothScrollLinks() {
+    // Add to nav links
     $('.main-nav-links a').smoothScroll({
         beforeScroll: closeNav,
+        speed: 600
+    });
+
+    // Add to contact link in bio
+    $('#bio-contact-link').smoothScroll({
         speed: 600
     });
 }
